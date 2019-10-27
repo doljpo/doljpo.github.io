88467c23b9d91b20c2e282a8ecd3ee4fd36e8302
David Oliveira <dol.jpo@hotmail.com>
2019-10-27 18:32:16 -0300

    Add javascript file

---
+++ script.js
@@ -0,0 +1,99 @@
+var message = document.querySelector("#message");
+var titulo = document.querySelector("h1");
+var gamePanels = document.querySelectorAll(".random-rgb-panel");
+var numberOfPanels = 6;
+
+generateRandomColors(numberOfPanels);
+
+var targetColor = document.querySelector("#targetColor");
+targetColor.textContent = selectedColor = pickColor();
+
+var newGameButton = document.querySelector("#newGameButton");
+newGameButton.addEventListener("click", function() {
+    newGame(numberOfPanels);
+});
+
+var easyModeButton = document.querySelector("#easyModeBtn");
+easyModeButton.addEventListener("click", function() {
+    this.classList.add("selectedMode");
+    hardModeButton.classList.remove("selectedMode");
+    numberOfPanels = 3;
+
+    newGame(numberOfPanels);
+});
+
+var hardModeButton = document.querySelector("#hardModeBtn");
+hardModeButton.addEventListener("click", function() {
+    this.classList.add("selectedMode");
+    easyModeButton.classList.remove("selectedMode");
+    numberOfPanels = 6;
+
+    newGame(numberOfPanels);
+});
+
+startGame();
+
+function startGame() {
+    for (let index = 0; index < gamePanels.length; index++) {
+        const panel = gamePanels[index];
+        
+        panel.addEventListener("click", function() {
+            var clickedColor = this.style.backgroundColor;
+    
+            if (clickedColor == selectedColor) {
+                message.textContent = "Correct";
+                endGame();
+            } else {
+                message.textContent = "Try Again";
+                this.style.backgroundColor = "#232323";
+            }
+        });
+    }    
+}
+
+function newGame(difficult) {
+    newGameButton.textContent = "NEW COLORS";
+    message.textContent = "";
+
+    generateRandomColors(difficult);
+    targetColor.textContent = selectedColor = pickColor();
+    titulo.style.backgroundColor = "#232323";
+}
+
+function generateRandomColors(numberOfPanels) {
+    for (let i = 0; i < numberOfPanels; i++) {
+        gamePanels[i].style.backgroundColor =
+         "rgb(" + randomTo255() + ", " + randomTo255() + " ," + randomTo255() + ")";
+    }
+
+    if (numberOfPanels < gamePanels.length) {
+        for (let index = numberOfPanels; index < gamePanels.length; index++) {
+            gamePanels[index].style.display = "none";
+        }
+    } else {
+        for (let index = 0; index < gamePanels.length; index++) {
+            gamePanels[index].style.display = "block";
+        }        
+    }
+}
+
+function randomTo255() {
+    return Math.floor(Math.random() * 255);
+}
+
+function pickColor() {
+    var randomColor = Math.floor(Math.random() * numberOfPanels);
+    return gamePanels[randomColor].style.backgroundColor;
+}
+
+function endGame() {
+    for (let index = 0; index < gamePanels.length; index++) {
+        const panel = gamePanels[index];
+        
+        panel.style.backgroundColor = selectedColor;
+    }
+
+    titulo.style.backgroundColor = selectedColor;
+    newGameButton.textContent = "PLAY AGAIN";
+}
+
